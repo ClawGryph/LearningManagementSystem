94860438b90367106e4f7f41a674f1053ff01b4f
ClawGryph <jess.secuya1213@gmail.com>
2025-07-03 20:00:11 +0800

    Add initial HTML structure for the login page

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div>
+        <h2>Cebu Technological University</h2>
+        <h3>Learning Management System</h3>
+        <!-- ICON !-->
+    </div>
+    <form action="" method="POST">
+        <h4>Choose your role</h4>
+        <select name="role" id="role">
+            <option value="select">Select Role</option>
+            <option value="admin">Admin</option>
+            <option value="instructor">Instructor</option>
+            <option value="student">Student</option>
+        </select>
+        <button type="submit">Confirm</button>
+    </form>
+</body>
+</html>
