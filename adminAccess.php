e632c9ebfb1b1f766cfaf4c70966f9c766bceeb4
ClawGryph <jess.secuya1213@gmail.com>
2025-07-03 21:54:34 +0800

    Add initial HTML structure for admin access page

---
+++ adminAccess.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin | Sign in</title>
+</head>
+<body>
+    <div>
+        <div>
+            <h2>Cebu Technological University</h2>
+            <h3>Learning Management System</h3>
+            <!-- ICON !-->
+        </div>
+        <div>
+            <h4>Sign in to your account</h4>
+            <form action="" method="POST">
+                <input type="email" id="e-mail" name="e-mail" placeholder="E-mail" required>
+                <input type="password" id="password" name="password" placeholder="Password" required>
+
+                <button type="submit">Sign in</button>
+            </form>
+        </div>
+        <div>
+            <h2>New here?</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus corrupti eum dolorum eligendi reiciendis alias sunt necessitatibus, id, non dolorem vel nihil perferendis? Mollitia cumque neque ab vitae impedit!
+            Numquam laboriosam libero officiis natus ex aspernatur, eum nam magnam minus cumque perspiciatis ea a ipsum placeat autem tempore praesentium ullam id. Id iste explicabo veritatis nemo aliquid corrupti recusandae?</p>
+            <button>Sign up</button>
+        </div>
+    </div>
+</body>
+</html>
