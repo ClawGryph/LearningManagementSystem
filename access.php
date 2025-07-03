2ea460d0d2207d0d85a7859bbf1a49031a3185dc
ClawGryph <jess.secuya1213@gmail.com>
2025-07-03 21:11:12 +0800

    Add HTML structure for Signin/Signup page

---
+++ access.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signin/Signup</title>
+</head>
+<body>
+    <!-- SIGN IN !-->
+    <div>
+        <div>
+            <h2>Cebu Technological University</h2>
+            <h3>Learning Management System</h3>
+            <!-- ICON !-->
+        </div>
+        <div>
+            <h4>Sign in to your account</h4>
+            <form action="" method="POST">
+                <input type="email" id="e-mail" name="e-mail" placeholder="E-mail" required>
+                <input type="password" id="password" name="password" placeholder="Password" required>
+
+                <button type="submit">Sign in</button>
+            </form>
+        </div>
+        <div>
+            <h2>New here?</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus corrupti eum dolorum eligendi reiciendis alias sunt necessitatibus, id, non dolorem vel nihil perferendis? Mollitia cumque neque ab vitae impedit!
+            Numquam laboriosam libero officiis natus ex aspernatur, eum nam magnam minus cumque perspiciatis ea a ipsum placeat autem tempore praesentium ullam id. Id iste explicabo veritatis nemo aliquid corrupti recusandae?</p>
+            <button>Sign up</button>
+        </div>
+    </div>
+
+    <!-- SIGN UP !-->
+    <div>
+        <div>
+            <h2>Welcome Back!</h2>
+            <p>To keep connected with us Sign in with your personal information</p>
+            <button>Sign in</button>
+        </div>
+        <div>
+            <h2>Create Accoune</h2>
+            <form action="" method="POST">
+                <input type="text" id="firstName" name="firstname" placeholder="First Name" required>
+                <input type="text" id="lastName" name="lastName" placeholder="Last Name" required>
+                <input type="email" id="email" name="email" placeholder="E-mail" required>
+                <input type="password" id="password" name="password" placeholder="Password" required>
+                <input type="password" id="confirmPass" name="confirmPass" placeholder="Re-enter Password" required>
+
+                <button type="submit">Sign up</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
