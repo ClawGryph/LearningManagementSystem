e54390abc575926041ab2ce6f6733cb60aa05c10
ClawGryph <jess.secuya1213@gmail.com>
2025-07-03 21:53:25 +0800

    Refactor HTML structure for improved semantics and organization

--- index.php
+++ index.php
@@ -4,22 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
-    <div>
-        <h2>Cebu Technological University</h2>
-        <h3>Learning Management System</h3>
-        <img src="./images/CTU-logo.png" alt="CTU Logo">
-    </div>
-    <form action="" method="POST">
-        <h4>Choose your role</h4>
-        <select name="role" id="role">
-            <option value="select">Select Role</option>
-            <option value="admin">Admin</option>
-            <option value="instructor">Instructor</option>
-            <option value="student">Student</option>
-        </select>
-        <button type="submit">Confirm</button>
-    </form>
+    <main>
+        <div>
+            <div>
+                <h2>Cebu Technological University</h2>
+                <h3>Learning Management System</h3>
+                <img src="./images/CTU-logo.png" alt="CTU Logo">
+            </div>
+            <div>
+                <form action="" method="POST">
+                    <h4>Choose your role</h4>
+                    <select name="role" id="role">
+                        <option value="select">Select Role</option>
+                        <option value="admin">Admin</option>
+                        <option value="instructor">Instructor</option>
+                        <option value="student">Student</option>
+                    </select>
+                    <button type="submit">Confirm</button>
+                </form>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
