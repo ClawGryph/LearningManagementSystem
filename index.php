92b99b876bb172db8ae27bcf6051b9d36b6ee1df
ClawGryph <jess.secuya1213@gmail.com>
2025-07-04 18:33:58 +0800

    Implement code changes to enhance functionality and improve performance

--- index.php
+++ index.php
@@ -3,30 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="/css/normalize.css">
-    <link rel="stylesheet" href="/css/styles.css">
+    <title>Choose Role</title>
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/styles.css">
 </head>
 <body>
-    <main>
-        <div>
-            <div>
-                <h2>Cebu Technological University</h2>
-                <h3>Learning Management System</h3>
-                <img src="./images/CTU-logo.png" alt="CTU Logo">
+    <main class="container wrapper">
+        <div class="role-card">
+            <div class="card-header">
+                <div>
+                    <h2>Cebu Technological University</h2>
+                    <h3>Learning Management System</h3>
+                </div>
+                <img src="./images/CTU-logo.png" class="logo" alt="CTU Logo">
             </div>
-            <div>
-                <form action="./action/roleSelection.php" method="POST">
-                    <h4>Choose your role</h4>
-                    <select name="role" id="role">
-                        <option value="select">Select Role</option>
-                        <option value="admin">Admin</option>
-                        <option value="instructor">Instructor</option>
-                        <option value="student">Student</option>
-                    </select>
-                    <button type="submit">Confirm</button>
-                </form>
-            </div>
+            <form action="./action/roleSelection.php" method="POST" class="card-body">
+                <label for="role">Choose a Role</label>
+                <select name="role" id="role">
+                    <option value="select">Select Role</option>
+                    <option value="admin">Admin</option>
+                    <option value="instructor">Instructor</option>
+                    <option value="student">Student</option>
+                </select>
+                <button type="submit">Confirm</button>
+            </form>
         </div>
     </main>
 </body>
