ad6b9848e54e71f86d1e2e284666474c0457e932
ClawGryph <jess.secuya1213@gmail.com>
2025-07-05 23:13:47 +0800

    Refactor admin sign-in page layout and styles; enhance form structure and add icons

--- adminAccess.php
+++ adminAccess.php
@@ -4,23 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin | Sign in</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/styles.css">
 </head>
 <body>
-    <div>
-        <div>
-            <h2>Cebu Technological University</h2>
-            <h3>Learning Management System</h3>
-            <!-- ICON !-->
-        </div>
-        <div>
-            <h4>Sign in to your account</h4>
-            <form action="" method="POST">
-                <input type="email" id="e-mail" name="e-mail" placeholder="E-mail" required>
-                <input type="password" id="password" name="password" placeholder="Password" required>
-
-                <button type="submit">Sign in</button>
+    <main class="container wrapper">
+        <div class="role-card">
+            <div class="card-header">
+                <div>
+                    <h2>Cebu Technological University</h2>
+                    <h3>Learning Management System</h3>
+                </div>
+                <img src="./images/CTU-logo.png" alt="CTU Logo" class="logo">
+            </div>
+            <form action="" method="POST" class="admin-form card-body">
+                <h4 class="title">Sign in to your account</h4>
+                <div class="input-field">
+                    <i class="fa-regular fa-envelope"></i>
+                    <input type="email" id="e-mail" name="e-mail" placeholder="E-mail" required>
+                </div>
+                <div class="input-field">
+                    <i class="fa-solid fa-lock"></i>
+                    <input type="password" id="password" name="password" placeholder="Password" required>
+                </div>
+                
+                <button type="submit" class="btn-drk-bg">Sign in</button>
             </form>
         </div>
-    </div>
+    </main>
 </body>
 </html>
