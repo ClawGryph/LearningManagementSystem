bdfef04698e153d88a7096563c44804802c6caac
ClawGryph <jess.secuya1213@gmail.com>
2025-07-07 22:45:05 +0800

    Add admin landing page with navigation and structure

---
+++ admin/admin-landingpage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin | Dashboard</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    <nav>
+        <div>
+            <img src="" alt="">
+        </div>
+        <ul>
+            <li><a href="#" data-content="admin-create-courses.php"><i class="fa-solid fa-circle-plus"></i>Create Courses</a></li>
+            <li><a href="#" data-content="admin-instructor-courses.php"><i class="fa-solid fa-user-plus"></i>Add Instructor to Courses</a></li>
+            <li><a href="#" data-content="admin-lm-lists"><i class="fa-solid fa-book-open"></i>Learning Materials Lists</a></li>
+            <li><a href="#" data-content="admin-notification"><i class="fa-solid fa-bell"></i>Notification</a></li>
+        </ul>
+    </nav>
+    <main>
+
+    </main>
+</body>
+</html>
