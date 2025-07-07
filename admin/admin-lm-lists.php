c04cf3f53b81d96f3b2b585a5d040af06875ff0b
ClawGryph <jess.secuya1213@gmail.com>
2025-07-07 23:17:19 +0800

    Add learning materials lists page with approval functionality

---
+++ admin/admin-lm-lists.php
@@ -0,0 +1,32 @@
+<div>
+    <div>
+        <!-- FIRST PAGE -->
+        <div>
+            <h2>Learning Materials Lists</h2>
+            <div>
+                <div>
+                    <p>
+                        <span><!-- Instructor's name --></span>
+                        added a course material to the course 
+                        <span><!-- Course name --></span>
+                    </p>
+                    <p>
+                        <span><i class="fa-solid fa-calendar-days"></i><!-- Date and time --></span>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- SECOND PAGE -->
+        <div>
+            <i class="fa-solid fa-circle-arrow-left"></i>
+            <div>
+                <!-- LIST SA MGA MA APPROVE NGA LM -->
+            </div>
+            <div>
+                <button type="submit">Approve</button>
+                <button type="submit">Decline</button>
+            </div>
+        </div>
+    </div>
+</div>
