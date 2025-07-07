afabf95913795d85f63eda69a1f88fd273a38b1e
ClawGryph <jess.secuya1213@gmail.com>
2025-07-07 23:03:38 +0800

    Add course creation page with form and table structure

---
+++ admin/admin-create-courses.php
@@ -0,0 +1,41 @@
+<div>
+    <div>
+        <!-- FIRST PAGE !-->
+        <div>
+            <h2>Create Courses</h2>
+            <div>
+                <button type="submit">Create new course</button>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Course Code</th>
+                        <th>Course Name</th>
+                        <th>Instructor</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+
+        <!-- SECOND PAGE !-->
+        <div>
+            <h2>Create New Course</h2>
+            <form action="" method="POST">
+                <div>
+                    <i class="fa-solid fa-book"></i>
+                    <input type="text" id="courseCode" name="courseCode" placeholder="Enter course code" required>
+                </div>
+                <div>
+                    <i class="fa-solid fa-keyboard"></i>
+                    <input type="text" id="courseName" name="courseName" placeholder="Enter course name" required>
+                </div>
+                <button type="submit">Create Course</button>
+            </form>
+        </div>
+    </div>
+</div>
