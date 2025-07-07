11bceeafbc81c4ff92280c91ec412afecd713c23
ClawGryph <jess.secuya1213@gmail.com>
2025-07-07 23:07:56 +0800

    Add instructor management interface with search functionality

---
+++ admin/admin-instructor-courses.php
@@ -0,0 +1,20 @@
+<div>
+    <div>
+        <!-- FIRST PAGE -->
+         <div>
+            <h2>Add Instructor</h2>
+
+            <div>
+                <input type="text" id="search-instructor" placeholder="Instructor Name ..."> 
+                <button id="search-instructor-btn"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+
+            <div>
+                <h3>Subjectc</h3>
+                <!-- RADIO BUTTONS FOR SUBJECTS -->
+
+                <button>Add</button>
+            </div>
+         </div>
+    </div>
+</div>
