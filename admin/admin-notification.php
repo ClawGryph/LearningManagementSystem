5cab3eeee94261de5d4642d21f74cc9f6a0e79d5
ClawGryph <jess.secuya1213@gmail.com>
2025-07-07 23:28:15 +0800

    Add admin notification page with course material updates

---
+++ admin/admin-notification.php
@@ -0,0 +1,31 @@
+<div>
+    <div>
+        <!-- FIRST PAGE -->
+        <div>
+            <div>
+                <h2>Welcome, Admin</h2>
+                <button type="submit">Logout</button>
+            </div>
+            <div>
+                <div>
+                    <button type="submit"><i class="fa-regular fa-circle-check"></i>Read</button>
+                    <input type="radio" id="checkAll" name="choices" value="checkAll">
+                    <label for="checkAll">Check All</label>
+                </div>
+                <div>
+                    <div>
+                        <!-- Radio Button -->
+                        <p>
+                            <span><!-- Instructor's name --></span>
+                            added a course material to the course 
+                            <span><!-- Course name --></span>
+                        </p>
+                    </div>
+                    <p>
+                        <span><i class="fa-solid fa-calendar-days"></i><!-- Date and time --></span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
